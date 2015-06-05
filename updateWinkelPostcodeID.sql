373cc4d2c341d78907fa2c92913a2c2f9f9c0184
salami350 <salami350@live.nl>
2015-06-05 23:50:36 +0200

    stuff2

--- updateWinkelPostcodeID.sql
+++ updateWinkelPostcodeID.sql
@@ -1,6 +1,26 @@
 CREATE OR REPLACE PROCEDURE updateWinkelPostcodeID
-IS
-  
+(
+  v_Winkel_postcode WINKEL.postcode%TYPE,
+  v_Winkel_postcode_id WINKEL.postcode_id%TYPE,
+  v_Postcode_postcode POSTCODE.postcode%TYPE,
+  v_Postcode_postcode_id POSTCODE.postcode_id%TYPE
+)
+AS
 BEGIN
+  SELECT postcode
+  FROM WINKEL
+  INTO v_Winkel_postcode;
   
+  SELECT postcode_id
+  FROM WINKEL
+  INTO v_Winkel_postcode_id;
+  
+  SELECT postcode
+  FROM POSTCODE
+  INTO v_Postcode_postcode;
+  
+  SELECT postcode_id
+  FROM POSTCODE
+  INTO v_Postcode_postcode_id;
 END updateWinkelPostcodeID;  
+/
